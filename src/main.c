9983edf2670da83fd57380784f5935f8b122a949
HenriqueIsonaka <henrique.isonaka@gmail.com>
2026-09-17 16:14:19 -0300

    beta para código 2.0

--- src/main.c
+++ src/main.c
@@ -15,9 +15,9 @@
 #define ADC_VREF_MV   3300
 #define DELAY_LDR_MS   500 // a ser definido
 #define TPM_MODULE 7500
-#define margem_LDR 110 // a ser definido
+#define margem_LDR 150 // a ser definido
 #define Delay_Leitura 2000 // a ser definido
-#define Noite   1400 // a ser definido
+#define Noite   200 // a ser definido
 #define Tempo_dormir  15000
 #define DELAY_viradona_MS 1000 
 
@@ -117,10 +117,14 @@ int main(void) {
     pwm_tpm_Init(TPM1, TPM_PLLFLL, TPM_MODULE, TPM_CLK, PS_128, EDGE_PWM);
     pwm_tpm_Init(TPM0, TPM_PLLFLL, TPM_MODULE, TPM_CLK, PS_128, EDGE_PWM);
     pwm_tpm_Ch_Init(TPM1, 0, TPM_PWM_H, GPIOA, 12); //PTA12-> servo da base
-    pwm_tpm_Ch_Init(TPM0, 0, TPM_PWM_H, GPIOC, 1);// PTC1-> servo do topo
+    pwm_tpm_Ch_Init(TPM0, 2, TPM_PWM_H, GPIOA, 5);// PTA5-> servo do topo
     pwm_tpm_CnV(TPM1, 0, meio);
     k_msleep(2000);
-    pwm_tpm_CnV(TPM0, 0, descanso);
+    pwm_tpm_CnV(TPM0, 2, meio);
+    uint16_t posicaoB = (movimento_b - zero)*0.24;
+    uint16_t posicaoT = (movimento_t - zero)*0.24;
+    printk("Posição servo base: %d graus\n",posicaoB);
+    printk("Posição servo topo: %d graus\n", posicaoT);
     k_msleep(2000);
 
     while (1) {
@@ -142,60 +146,98 @@ int main(void) {
             printk("Falha na leitura do ADC: %d\n", err4);
         }
         else {
+
             int32_t Nmv = sample_buffer_N;
             int32_t Smv = sample_buffer_S;
             int32_t Lmv = sample_buffer_L;
             int32_t Omv = sample_buffer_O;
+
             adc_raw_to_millivolts (ADC_VREF_MV, ADC_GAIN, ADC_RESOLUTION, &Nmv);
             adc_raw_to_millivolts (ADC_VREF_MV, ADC_GAIN, ADC_RESOLUTION, &Smv);
             adc_raw_to_millivolts (ADC_VREF_MV, ADC_GAIN, ADC_RESOLUTION, &Lmv);
             adc_raw_to_millivolts (ADC_VREF_MV, ADC_GAIN, ADC_RESOLUTION, &Omv);
 
-            uint32_t VN = Nmv-Smv;
-            uint32_t VS = Smv;
-            uint32_t VL = Lmv-Omv;
-            uint32_t VO = Omv;
+            uint32_t VN = Smv;// leitura adc le o resistor e nao o ldr
+            uint32_t VS = Nmv;
+            uint32_t VL = Omv;
+            uint32_t VO = Lmv;
 
             printk("ADC Norte: %d (raw), %d mV\n", sample_buffer_N, VN);
             printk("ADC Sul: %d (raw), %d mV\n", sample_buffer_S, VS);
             printk("ADC Leste: %d (raw), %d mV\n", sample_buffer_L, VL);
             printk("ADC Oeste: %d (raw), %d mV\n", sample_buffer_O, VO);
             printk("\n");
+            
+            
+            if(VN < Noite && VS < Noite) {
+                movimento_t = descanso;
+                movimento_b = meio;
+                pwm_tpm_CnV(TPM0, 2, movimento_t);
+                k_msleep(1000);
+                pwm_tpm_CnV(TPM1, 0, movimento_b);
+                posicaoB = (movimento_b - zero)*0.24;
+                posicaoT = (movimento_t - zero)*0.24;
+                printk("Posição servo base: %d graus\n",posicaoB);
+                printk("Posição servo topo: %d graus\n", posicaoT);
+                printk("Dormindo...\n");
+                printk("\n");
+                k_msleep(Tempo_dormir);
+            }
 
             if(VL> VO+margem_LDR) { //vai para oeste
                 if(movimento_t> meio && movimento_b<max) {
                     movimento_b = movimento_b+ mexida;
                     pwm_tpm_CnV(TPM1, 0, movimento_b);
+                    posicaoB = (movimento_b - zero)*0.24;
+                    posicaoT = (movimento_t - zero)*0.24;
+                    printk("Posição servo base: %d graus\n",posicaoB);
+                    printk("Posição servo topo: %d graus\n", posicaoT);
+                    printk("\n");
                     k_msleep(DELAY_LDR_MS);
                 }
                 else if(movimento_t> meio && movimento_b>=max) {
                     movimento_t = meio;
                     movimento_b = meio;
-                    pwm_tpm_CnV(TPM0, 0, movimento_t);
+                    pwm_tpm_CnV(TPM0, 2, movimento_t);
                     pwm_tpm_CnV(TPM1, 0, movimento_b);
                     k_msleep(DELAY_viradona_MS);
                     movimento_t = descanso;
                     movimento_b = zero+mexida;
-                    pwm_tpm_CnV(TPM0, 0, movimento_t);
+                    pwm_tpm_CnV(TPM0, 2, movimento_t);
                     pwm_tpm_CnV(TPM1, 0, movimento_b);
+                    posicaoB = (movimento_b - zero)*0.24;
+                    posicaoT = (movimento_t - zero)*0.24;
+                    printk("Posição servo base: %d graus\n",posicaoB);
+                    printk("Posição servo topo: %d graus\n", posicaoT);
+                    printk("\n");
                     k_msleep(DELAY_viradona_MS);
                     //travada = true;
                 }
                 else if(movimento_t< meio && movimento_b>zero) {
                     movimento_b = movimento_b- mexida;
                     pwm_tpm_CnV(TPM1, 0, movimento_b);
+                    posicaoB = (movimento_b - zero)*0.24;
+                    posicaoT = (movimento_t - zero)*0.24;
+                    printk("Posição servo base: %d graus\n",posicaoB);
+                    printk("Posição servo topo: %d graus\n", posicaoT);
+                    printk("\n");
                     k_msleep(DELAY_LDR_MS);
                 }
                 else if(movimento_t< meio && movimento_b<=zero) {
                     movimento_t = meio;
                     movimento_b = meio;
-                    pwm_tpm_CnV(TPM0, 0, movimento_t);
+                    pwm_tpm_CnV(TPM0, 2, movimento_t);
                     pwm_tpm_CnV(TPM1, 0, movimento_b);
                     k_msleep(DELAY_viradona_MS);
                     movimento_t = outdesc;
                     movimento_b = max-mexida;
-                    pwm_tpm_CnV(TPM0, 0, movimento_t);
+                    pwm_tpm_CnV(TPM0, 2, movimento_t);
                     pwm_tpm_CnV(TPM1, 0, movimento_b);
+                    posicaoB = (movimento_b - zero)*0.24;
+                    posicaoT = (movimento_t - zero)*0.24;
+                    printk("Posição servo base: %d graus\n",posicaoB);
+                    printk("Posição servo topo: %d graus\n", posicaoT);
+                    printk("\n");
                     k_msleep(DELAY_viradona_MS);
                     //travada = true;
                 }
@@ -204,36 +246,56 @@ int main(void) {
                 if(movimento_t> meio && movimento_b>zero) {
                     movimento_b = movimento_b- mexida;
                     pwm_tpm_CnV(TPM1, 0, movimento_b);
+                    posicaoB = (movimento_b - zero)*0.24;
+                    posicaoT = (movimento_t - zero)*0.24;
+                    printk("Posição servo base: %d graus\n",posicaoB);
+                    printk("Posição servo topo: %d graus\n", posicaoT);
+                    printk("\n");
                     k_msleep(DELAY_LDR_MS);
                 }
                 else if(movimento_t> meio && movimento_b<=zero) {
                     movimento_t = meio;
                     movimento_b = meio;
-                    pwm_tpm_CnV(TPM0, 0, movimento_t);
+                    pwm_tpm_CnV(TPM0, 2, movimento_t);
                     pwm_tpm_CnV(TPM1, 0, movimento_b);
                     k_msleep(DELAY_viradona_MS);
                     movimento_t = descanso;
                     movimento_b = max-mexida;
-                    pwm_tpm_CnV(TPM0, 0, movimento_t);
+                    pwm_tpm_CnV(TPM0, 2, movimento_t);
                     pwm_tpm_CnV(TPM1, 0, movimento_b);
+                    posicaoB = (movimento_b - zero)*0.24;
+                    posicaoT = (movimento_t - zero)*0.24;
+                    printk("Posição servo base: %d graus\n",posicaoB);
+                    printk("Posição servo topo: %d graus\n", posicaoT);
+                    printk("\n");
                     k_msleep(DELAY_viradona_MS);
                     //travada = true;
                 }
                 else if(movimento_t< meio && movimento_b<max) {
                     movimento_b = movimento_b+ mexida;
                     pwm_tpm_CnV(TPM1, 0, movimento_b);
+                    posicaoB = (movimento_b - zero)*0.24;
+                    posicaoT = (movimento_t - zero)*0.24;
+                    printk("Posição servo base: %d graus\n",posicaoB);
+                    printk("Posição servo topo: %d graus\n", posicaoT);
+                    printk("\n");
                     k_msleep(DELAY_LDR_MS);
                 }
                 else if(movimento_t < meio && movimento_b>=max) {
                     movimento_t = meio;
                     movimento_b = meio;
-                    pwm_tpm_CnV(TPM0, 0, movimento_t);
+                    pwm_tpm_CnV(TPM0, 2, movimento_t);
                     pwm_tpm_CnV(TPM1, 0, movimento_b);
                     k_msleep(DELAY_viradona_MS);
                     movimento_t = outdesc;
                     movimento_b = zero+mexida;
-                    pwm_tpm_CnV(TPM0, 0, movimento_t);
+                    pwm_tpm_CnV(TPM0, 2, movimento_t);
                     pwm_tpm_CnV(TPM1, 0, movimento_b);
+                    posicaoB = (movimento_b - zero)*0.24;
+                    posicaoT = (movimento_t - zero)*0.24;
+                    printk("Posição servo base: %d graus\n",posicaoB);
+                    printk("Posição servo topo: %d graus\n", posicaoT);
+                    printk("\n");
                     k_msleep(DELAY_viradona_MS);
                     //travada = true;
                 }
@@ -241,25 +303,32 @@ int main(void) {
 
             else if(VN> VS+margem_LDR) { //vai para sul
                 movimento_t = movimento_t - mexida;
-                pwm_tpm_CnV(TPM0, 0, movimento_t);
+                pwm_tpm_CnV(TPM0, 2, movimento_t);
+                posicaoB = (movimento_b - zero)*0.24;
+                posicaoT = (movimento_t - zero)*0.24;
+                printk("Posição servo base: %d graus\n",posicaoB);
+                printk("Posição servo topo: %d graus\n", posicaoT);
+                printk("\n");
                 k_msleep(DELAY_LDR_MS);
             }
             else if(VS> VN+margem_LDR) { //vai para norte
                 movimento_t = movimento_t + mexida;
-                pwm_tpm_CnV(TPM0, 0, movimento_t);
+                pwm_tpm_CnV(TPM0, 2, movimento_t);
+                posicaoB = (movimento_b - zero)*0.24;
+                posicaoT = (movimento_t - zero)*0.24;
+                printk("Posição servo base: %d graus\n",posicaoB);
+                printk("Posição servo topo: %d graus\n", posicaoT);
+                printk("\n");
                 k_msleep(DELAY_LDR_MS);
             }
             else {
+                posicaoB = (movimento_b - zero)*0.24;
+                posicaoT = (movimento_t - zero)*0.24;
+                printk("Posição servo base: %d graus\n",posicaoB);
+                printk("Posição servo topo: %d graus\n", posicaoT);
+                printk("\n");
                 k_msleep(Delay_Leitura);
                 //travada = false;
-            }
-            if(VN> Noite && VS>Noite) {
-                pwm_tpm_CnV(TPM0, 0, descanso);
-                k_msleep(1000);
-                pwm_tpm_CnV(TPM1, 0, meio);
-                k_msleep(Tempo_dormir);
-                movimento_t = descanso;
-                movimento_b = meio;
             }
         }
     }
@@ -273,12 +342,9 @@ int main(void) {
 // Motor base: PTA 12
 // Motor topo: PTC 1->trocar por tranco
 // Lembrar de definir posição dos LDR com base nas posições de descanso
-// iiii: otimização do código
 // aaaaa:resolver tranco inicial trocando entrada
 // eeeee: ajustar margem ldr norte e sul
 // eeeee: ajustar noite e margem ldr
 // ccccc:resolver viradona-> se retornar para viradona ele trava na posição
 // ccccc: quebrar movimento de viradona em dois forçando ele para a posição de meio
-
-
-
+// ccccc: ajustar para o circuito novo
